9c36677bdda1f63b42aece714abbbd586fdb77b4
rystroh <rystroh@dream.fr>
2023-02-21 10:56:59 +0100

    initial commit

---
+++ Source/NewComponent.cpp
@@ -0,0 +1,169 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 6.1.6
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2020 - Raw Material Software Limited.
+
+  ==============================================================================
+*/
+
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+
+#include "NewComponent.h"
+
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
+//==============================================================================
+NewComponent::NewComponent ()
+{
+    //[Constructor_pre] You can add your own custom stuff here..
+    //[/Constructor_pre]
+
+    juce__slider.reset (new juce::Slider ("new slider"));
+    addAndMakeVisible (juce__slider.get());
+    juce__slider->setRange (0, 10, 0);
+    juce__slider->setSliderStyle (juce::Slider::LinearVertical);
+    juce__slider->setTextBoxStyle (juce::Slider::TextBoxAbove, false, 50, 20);
+    juce__slider->addListener (this);
+
+    juce__slider->setBounds (120, 264, 55, 232);
+
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    setSize (600, 400);
+
+
+    //[Constructor] You can add your own custom stuff here..
+    //[/Constructor]
+}
+
+NewComponent::~NewComponent()
+{
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+    juce__slider = nullptr;
+
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
+}
+
+//==============================================================================
+void NewComponent::paint (juce::Graphics& g)
+{
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
+
+    g.fillAll (juce::Colour (0xff323e44));
+
+    {
+        int x = 12, y = 12, width = 775, height = 200;
+        juce::Colour fillColour = juce::Colours::black;
+        //[UserPaintCustomArguments] Customize the painting arguments here..
+        //[/UserPaintCustomArguments]
+        g.setColour (fillColour);
+        g.fillRect (x, y, width, height);
+    }
+
+    {
+        int x = 20, y = 244, width = 172, height = 276;
+        juce::Colour fillColour1 = juce::Colour (0xff828282), fillColour2 = juce::Colour (0xff414341);
+        juce::Colour strokeColour = juce::Colour (0xffb1b1b1);
+        //[UserPaintCustomArguments] Customize the painting arguments here..
+        //[/UserPaintCustomArguments]
+        g.setGradientFill (juce::ColourGradient (fillColour1,
+                                             0.0f - 20.0f + x,
+                                             static_cast<float> (-16) - 244.0f + y,
+                                             fillColour2,
+                                             264.0f - 20.0f + x,
+                                             376.0f - 244.0f + y,
+                                             false));
+        g.fillRect (x, y, width, height);
+        g.setColour (strokeColour);
+        g.drawRect (x, y, width, height, 1);
+
+    }
+
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
+}
+
+void NewComponent::resized()
+{
+    //[UserPreResize] Add your own custom resize code here..
+    //[/UserPreResize]
+
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
+}
+
+void NewComponent::sliderValueChanged (juce::Slider* sliderThatWasMoved)
+{
+    //[UsersliderValueChanged_Pre]
+    //[/UsersliderValueChanged_Pre]
+
+    if (sliderThatWasMoved == juce__slider.get())
+    {
+        //[UserSliderCode_juce__slider] -- add your slider handling code here..
+        //[/UserSliderCode_juce__slider]
+    }
+
+    //[UsersliderValueChanged_Post]
+    //[/UsersliderValueChanged_Post]
+}
+
+
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+//[/MiscUserCode]
+
+
+//==============================================================================
+#if 0
+/*  -- Projucer information section --
+
+    This is where the Projucer stores the metadata that describe this GUI layout, so
+    make changes in here at your peril!
+
+BEGIN_JUCER_METADATA
+
+<JUCER_COMPONENT documentType="Component" className="NewComponent" componentName=""
+                 parentClasses="public juce::Component" constructorParams="" variableInitialisers=""
+                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
+                 fixedSize="0" initialWidth="600" initialHeight="400">
+  <BACKGROUND backgroundColour="ff323e44">
+    <RECT pos="12 12 775 200" fill="solid: ff000000" hasStroke="0"/>
+    <RECT pos="20 244 172 276" fill="linear: 0 -16, 264 376, 0=ff828282, 1=ff414341"
+          hasStroke="1" stroke="1, mitered, butt" strokeColour="solid: ffb1b1b1"/>
+  </BACKGROUND>
+  <SLIDER name="new slider" id="e5d6a49b6edc8d01" memberName="juce__slider"
+          virtualName="" explicitFocusOrder="0" pos="120 264 55 232" min="0.0"
+          max="10.0" int="0.0" style="LinearVertical" textBoxPos="TextBoxAbove"
+          textBoxEditable="1" textBoxWidth="50" textBoxHeight="20" skewFactor="1.0"
+          needsCallback="1"/>
+</JUCER_COMPONENT>
+
+END_JUCER_METADATA
+*/
+#endif
+
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
